d6114bf67a0b079b3dd70c9d6e4ad6a020471f3a
Efimov Nikita <ne24kit@gmail.com>
2023-09-07 14:00:27 +0300

    Second version of the structure of storing file data as a one-dimensional array

--- text.cpp
+++ text.cpp
@@ -3,20 +3,29 @@
 #define MAX_LEN_STR 51
 #define MAX_NUM_STR 20
 
-int ReadFile(FILE * fp, char text[][MAX_LEN_STR])
+char * AddressElem(char * data, int j, int i);
+
+int ReadFile(FILE * fp, char * text);
+void PrintText(char * text, int size);
+
+char * AddressElem(char * data, int j, int i)
+{
+	return data + j * MAX_LEN_STR + i;
+}
+
+int ReadFile(FILE * fp, char * text)
 {
     int size = 0;
     
-    while(fgets(text[size], MAX_LEN_STR, fp) != NULL)
+    while(fgets(AddressElem(text, size, 0), MAX_LEN_STR, fp) != NULL)
         size++;
-    
     return size;
 }
 
-void PrintText(char text[][MAX_LEN_STR], int size)
+void PrintText(char * text, int size)
 {
     for(int i = 0; i < size; i++)
-        printf("%s", text[i]);
+        printf("%s", AddressElem(text, i, 0));
 
     printf("\n");
 }
@@ -37,9 +46,11 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    char text[MAX_NUM_STR][MAX_LEN_STR] = {};
+    char *text = (char *)calloc(MAX_NUM_STR, sizeof(char) * MAX_LEN_STR);
 
     PrintText(text, ReadFile(fp, text));
+
+    free(text);
 
     if (fclose(fp) != 0)
         fprintf(stderr, "File close error: <%s>\n", filename);
